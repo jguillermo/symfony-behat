c822f37e3008455decf1121840a73e3cae993b7b
Jose Guillermo <jguillermo@outlook.com>
2021-03-22 10:10:00 -0500

    add-behat

--- src/Controller/HealthCheckController.php
+++ src/Controller/HealthCheckController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 
 
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HealthCheckController
@@ -17,6 +18,14 @@ class HealthCheckController
         return new JsonResponse([
             'status'=>'ok'
         ]);
+    }
+
+    /**
+     * @Route("/ok", methods={"GET"})
+     */
+    public function ok()
+    {
+        return new Response("");
     }
 
 }
